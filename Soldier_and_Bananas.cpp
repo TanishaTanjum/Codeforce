17c0d502ba2436dec1163be52b4186bca5746463
TanishaTanjum <tanisha0162@gmail.com>
2025-12-07 15:21:39 +0600

    Add files via upload

---
+++ Soldier_and_Bananas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k, n, w , totalCost = 0;
+        cin >> k >> n >> w;
+    for(int i=1; i<=w; i++) {
+        totalCost += k*i ;
+    }
+    if(totalCost > n) {
+        cout << totalCost-n << endl;
+    }
+    else {
+        cout << 0 <<endl;
+    }
+
+}
